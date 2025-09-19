6abfe22d2a456ac07306484d6dcc52690817ee76
blachorz <blachorz.m@gmail.com>
2025-09-19 12:30:21 +0800

    feat: add 'Brain Science of Studying' book notes

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,8 @@ export default defineConfig({
         main: resolve(__dirname, 'index.html'),
         book1: resolve(__dirname, 'book-how-to-raise-social-child.html'),
         book2: resolve(__dirname, 'book-unconscious-obedience-and-awareness.html'),
-        book3: resolve(__dirname, 'book-you-are-your-childs-best-toy.html')
+        book3: resolve(__dirname, 'book-you-are-your-childs-best-toy.html'),
+        book4: resolve(__dirname, 'book-brain-science-of-studying.html')
       }
     }
   }
