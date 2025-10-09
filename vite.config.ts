e6279090e5f4421985c7ed95facbc1b5690ddc25
blachorz <blachorz.m@gmail.com>
2025-10-09 11:56:19 +0800

    feat: Add Enterprise Evolution Theory book page

--- vite.config.ts
+++ vite.config.ts
@@ -41,6 +41,7 @@ export default defineConfig({
         'book-peak': resolve(__dirname, 'bookshelf/book-peak.html'),
         'book-replicable-leadership': resolve(__dirname, 'bookshelf/book-replicable-leadership.html'),
         'book-strengthen-your-vagus-nerve': resolve(__dirname, 'bookshelf/book-strengthen-your-vagus-nerve.html'),
+        'book-enterprise-evolution-theory': resolve(__dirname, 'bookshelf/book-enterprise-evolution-theory.html'),
       }
     }
   }
